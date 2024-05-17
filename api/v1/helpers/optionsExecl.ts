ef2def051bd7459468a7fc6c884fb50aeefd391d
doduong <dokhacduong3@gmail.com>
2024-05-16 21:04:17 -0700

    update-9

--- api/v1/helpers/optionsExecl.ts
+++ api/v1/helpers/optionsExecl.ts
@@ -1,17 +1,17 @@
 export var optionsExecl = {
     margins: {
-      left: 0.16,
+      left: 1.52,
       right: 0.24,
-      top: 1.01,
-      bottom: 0.75,
-      header: 0.3,
-      footer: 0.3,
+      top: 0,
+      bottom: 1.25,
+      header: 0,
+      footer: 0,
     },
     sheetFormat: {
       thickBottom: false,
       thickTop: false,
-      baseColWidth: 7,
-      defaultColWidth: 7,
+      baseColWidth: 9,
+      defaultColWidth: 9,
     },
     printOptions: {
       centerHorizontal: true,
